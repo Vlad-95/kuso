5d58a5b88d36f08cdf92e1a9a52275142b341f96
Vladislav <vzv@siluetstudio.com>
2021-10-22 15:15:15 +0300

    page

--- js/jquery-ready.js
+++ js/jquery-ready.js
@@ -57,130 +57,37 @@ $(document).ready(function() {
 
     //=====Якорные ссылки КОНЕЦ==
 
-    //========ПРОДУКЦИЯ ГЕКСЫ
-    let productItem = $('.product__item:not(.product__item_empty), .footer .nav__item');
-    let productBlock = $('.product-detail');
-    let productCloseBtn = $('.product-detail__btns .close');
-
-    // переменные для скролла
-    let allProductItems = $('.product__item');
-
-    // анимация закрашивания
-    $(window).scroll(function() {
-        
-        allProductItems.each(function() {
-            if (($(this).offset().top - $(window).scrollTop()) < windowHeight / 2) {
-                $(this).addClass('anim');
-            }
-            
+    //=====Слайдеры в блоке УСТАНОВКА======
+    if ($('.install .js-slider').length) {
+        $('.install .js-slider').slick({
+            slidesToShow: 1,
+            dots: true,
+            arrows: false
         })
-    })
-
-    // функция показа соответствующейго блока
-    function showProductItem () {
-        let currentItem = $(this);
-        let currentItemData = $(this).attr('data-item');
-        let productItemContent = $('.product-detail__item[data-item="'+ currentItemData +'"]');
-
-        currentItem.addClass('active').siblings().removeClass('active');
-        productBlock.fadeIn();
-        productItemContent.fadeIn().siblings().hide();
-
-        //скролл до элемента
-        $('html, body').animate({scrollTop: productBlock.offset().top}, 300)
-
     }
+    //=====Слайдеры в блоке УСТАНОВКА КОНЕЦ==============
 
-    // функция закрытия блока
-    function hideProductItem () {
-        let closeBtn = $(this);
-        let productItemContent = $(this).closest('.product-detail__item');
-        let productItemContentData = productItemContent.attr('data-item');
+    //=====Слайдер Галерея=========
+    if($('.gallery').length) {
+        // проверка на инициализацию работает только перед привязки слайдера
+        $('.js-gallery').on('init', function(event, slick){
+            console.log('init');
+        });
 
-        productItemContent.slideUp();
-        productBlock.slideUp();
-        productItem.removeClass('active');
-    }
+        $('.js-gallery').slick({
+            dots: false,
+            slidesToShow: 1,
+            variableWidth: true,
+            infinite: false,
+            arrows: true
+        });
 
-    productItem.click(showProductItem);
-    productCloseBtn.click(hideProductItem);
+        // $('.js-gallery').on('edge', function(event, slick, direction){
+        //     console.log('edge was hit')
+        // });
 
-    //========ПРОДУКЦИЯ ГЕКСЫ (КОНЕЦ)
-
-    //========Преимущества=====
-    
-    //открытие по клику
-    function showAdvantage () {
-        advantageFlag = true;
-        let currentAdvantage = $(this).closest('.advantages__item');
-        let currentAdvantageData = currentAdvantage.attr('data-item');
-        let advantagesContent = $('.advantages-content');
-        let currentAdvantageContent = $('.advantages-content__item[data-item="'+ currentAdvantageData +'"]');
-
-        $('html, body').animate({scrollTop: $('.advantages').offset().top}, 300)
-        currentAdvantage.removeClass('opacity').siblings().not('.advantages__item_main').addClass('opacity');
-        advantagesContent.fadeIn();
-        currentAdvantageContent.fadeIn().siblings('.advantages-content__item').hide();
-        //пагинация
-        $('.pagination__item').eq(currentAdvantage.index() - 1).addClass('active');
-    }
-
-    //закрытие по лику
-    function hideAdvantage () {        
-        let advantagesContent = $('.advantages-content');
-        let advantagesContentItem = $('.advantages-content__item');
-        
-        $('.advantages__item').removeClass('opacity');
-        $('.pagination__item').removeClass('active');
-        advantagesContent.fadeOut();
-        advantagesContentItem.fadeOut();
         
     }
 
-    // пагинация
-    function advantagesPag () {
-        let targetPage = $(this).not('.active');
-        let targetPageData = targetPage.attr('data-item');
-        let targetAdvantagesContentItem = $('.advantages-content__item[data-item="'+ targetPageData +'"]');
-
-        targetPage.addClass('active').siblings().removeClass('active');
-        targetAdvantagesContentItem.fadeIn().siblings('.advantages-content__item').hide();
-
-    }
-
-    $('.advantages-content .close').click(hideAdvantage);
-
-    $('.advantages__item .btn').click(showAdvantage);
-    
-    $('.pagination__item').click(advantagesPag);
-
-    //========Преимущества КОНЕЦ=========
-
-    //паралакс эффект
-    function parallax (evt) {
-        let parallaxItem = $('.parallax');
-        let scrolled = $(window).scrollTop();
-
-        parallaxItem.each(function() {
-            let currentItemDelay = $(this).attr('data-parallax-delay')
-            
-            $(this).css('top',(0 - (scrolled*currentItemDelay))+'px');
-        })
-    }
-
-    // if ($(window).width() >= 992) {
-    //     $(window).scroll(parallax);
-    // }
-
-    
-
-    // появление элементов
-    $(window).scroll(function () {
-        $('.fade').each(function() {
-            if (($(this).offset().top - $(window).scrollTop()) < windowHeight / 2) {
-                $(this).addClass('anim');
-            }
-        })
-    })
-
+    //=====Слайдер Галерея КОНЕЦ========
 });
